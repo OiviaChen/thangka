f7b11ef392c526f5922f731a3da834478916ce0c
OiviaChen <34407317+OiviaChen@users.noreply.github.com>
2020-03-04 10:13:02 -0500

    Add files via upload
    ver 1.0 
    -Create the template

---
+++ thesisproject_code/js/main.js
@@ -0,0 +1,13 @@
+//get all elements from menu
+const allItems = document.querySelectorAll(".navmenu ul li a");
+allItems.forEach(item =>{
+    item.addEventListener("click", function(e){
+        // here we will write loop remove all prevois classes active
+        for(var i = 0; i < allItems.length; i++){
+            allItems[i].classList.remove("active");
+            
+        }
+        this.classList.add("active");
+    })
+})
+
